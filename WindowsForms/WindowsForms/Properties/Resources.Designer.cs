48ff170d0b3b480d9443981f3e05044f8c96f042
Elisabeth Volkinshtein <volkinshteinel84659@th-nuernberg.de>
2021-11-27 13:21:26 +0100

    Health bar Restart(not done) Game over mechnism

--- WindowsForms/WindowsForms/Properties/Resources.Designer.cs
+++ WindowsForms/WindowsForms/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace WindowsForms.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap images {
+            get {
+                object obj = ResourceManager.GetObject("images", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Player {
             get {
                 object obj = ResourceManager.GetObject("Player", resourceCulture);
